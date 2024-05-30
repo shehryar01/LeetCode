ca20c600250d71b6837deb359533f00a2c50b537
shehryar01 <73014167+shehryar01@users.noreply.github.com>
2024-05-30 16:20:59 +0500

    [1071] [Time Beats: 100.00%] [Memory Beats: 96.38%] - LeetPush

---
+++ 1071-Greatest-Common-Divisor-of-Strings.swift
@@ -0,0 +1,16 @@
+class Solution {
+    func gcdOfStrings(_ str1: String, _ str2: String) -> String {
+        guard (str1 + str2) == (str2 + str1) else { return "" }
+        
+        let gcd = gcd(str1.count, str2.count)
+        return String(str1.prefix(gcd))
+    }
+
+    func gcd(_ x: Int, _ y: Int) -> Int {
+        if y == 0 {
+            return x;
+        } else {
+            return gcd(y, x % y);
+        }
+    }
+}
